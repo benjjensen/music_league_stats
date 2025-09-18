16b556f211f4e5c6207df7efd30f787d066582d5
Benj Jensen <benjjensen@live.com>
2025-09-17 20:47:56 -0400

    Initial draft, with data JSON files

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/services.dart' show rootBundle; 
-import 'package:csv/csv.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,12 +28,14 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
+  final List<String> leagues = const ["virginia_is_for_music_lovers", "live_the_riv", "granny_smith"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
       appBar: buildMainAppBar("Music Leagues"), 
       body: ListView(  
-        children: leagueRounds.keys.map( (league) {
+        children: leagues.map( (league) {
           return Card(  
             child: ListTile(  
               title: Text(league), 
@@ -39,7 +43,8 @@ class HomePage extends StatelessWidget {
                 Navigator.push( 
                   context,  
                   MaterialPageRoute( 
-                    builder: (context) => LeaguePage(leagueName: league), 
+                    // builder: (context) => LeaguePage(leagueName: league),
+                    builder: (context) => StatsPage(leagueName: league),  
                   ),
                 );
               },
@@ -62,78 +67,76 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class LeaguePage extends StatefulWidget {
-  final String leagueName; 
-  const LeaguePage({super.key, required this.leagueName});
+// class LeaguePage extends StatefulWidget {
+//   final String leagueName; 
+//   const LeaguePage({super.key, required this.leagueName});
 
-  @override
-  State<LeaguePage> createState() => _LeaguePageState(); 
-}
+//   @override
+//   State<LeaguePage> createState() => _LeaguePageState(); 
+// }
 
-class _LeaguePageState extends State<LeaguePage> {
-  String? selectedRound;
-  Future<List<List<dynamic>>>? data;
+// class _LeaguePageState extends State<LeaguePage> {
+//   Future<List<List<dynamic>>>? data;
 
-  void _loadRound(String roundFile) {
-    setState(() {
-      selectedRound = roundFile;
-      data = loadLeagueRound(widget.leagueName, roundFile);
-    });
-  }
+//   void _loadRound(String roundFile) {
+//     setState(() {
+//       data = loadLeagueRound(widget.leagueName, roundFile);
+//     });
+//   }
 
-  void _backToRounds() {
-    setState(() {
-      selectedRound = null;
-      data = null;
-    });
-  }
+//   void _backToRounds() {
+//     setState(() {
+//       selectedRound = null;
+//       data = null;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    final rounds = leagueRounds[widget.leagueName]!;
+//   @override
+//   Widget build(BuildContext context) {
+//     final rounds = leagueRounds[widget.leagueName]!;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.leagueName),
-        actions: [
-          if (selectedRound != null)
-            IconButton(
-              icon: const Icon(Icons.refresh), // "back to round picker"
-              onPressed: _backToRounds,
-              tooltip: "Back to rounds",
-            ),
-        ],
-      ),
-      body: selectedRound == null
-          ? ListView(
-              children: rounds.map((round) {
-                return ListTile(
-                  title: Text(round.replaceAll('.csv', '')),
-                  onTap: () => _loadRound(round),
-                );
-              }).toList(),
-            )
-          : FutureBuilder<List<List<dynamic>>>(
-              future: data,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                final rows = snapshot.data!;
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.leagueName),
+//         actions: [
+//           if (selectedRound != null)
+//             IconButton(
+//               icon: const Icon(Icons.refresh), // "back to round picker"
+//               onPressed: _backToRounds,
+//               tooltip: "Back to rounds",
+//             ),
+//         ],
+//       ),
+//       body: selectedRound == null
+//           ? ListView(
+//               children: rounds.map((round) {
+//                 return ListTile(
+//                   title: Text(round.replaceAll('.csv', '')),
+//                   onTap: () => _loadRound(round),
+//                 );
+//               }).toList(),
+//             )
+//           : FutureBuilder<List<List<dynamic>>>(
+//               future: data,
+//               builder: (context, snapshot) {
+//                 if (!snapshot.hasData) {
+//                   return const Center(child: CircularProgressIndicator());
+//                 }
+//                 final rows = snapshot.data!;
 
-                return ListView(
-                  children: rows.map((row) {
-                    return ListTile(
-                      title: Text(row[0].toString()), // voter/submitter
-                      subtitle: Text(row.skip(1).join(", ")), // their votes
-                    );
-                  }).toList(),
-                );
-              },
-            ),
-    );
-  }
-}
+//                 return ListView(
+//                   children: rows.map((row) {
+//                     return ListTile(
+//                       title: Text(row[0].toString()), // voter/submitter
+//                       subtitle: Text(row.skip(1).join(", ")), // their votes
+//                     );
+//                   }).toList(),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
 
 // class _LeaguePageStart extends State<LeaguePage> {
 //   late Future<List<List<dynamic>>> _data; 
@@ -195,85 +198,139 @@ class _LeaguePageState extends State<LeaguePage> {
 //   }
 // }
 
-class StatsPage extends StatelessWidget { 
+class StatsPage extends StatefulWidget { 
   final String leagueName; 
-  final String playerName; 
-  final points = [10, 9, 12, 0, -1, 8, 20];
 
-  StatsPage({required this.leagueName, required this.playerName}); 
+  const StatsPage({super.key, required this.leagueName}); 
+
+  @override  
+  State<StatsPage> createState() => _StatsPageState(); 
+}
+
+class _StatsPageState extends State<StatsPage> {
+  late Future<Map<String, dynamic>> _data; 
+
+  @override 
+  void initState() {
+    super.initState(); 
+    _data = loadStatsJson(widget.leagueName);
+  }
 
   @override 
   Widget build(BuildContext context) { 
     return Scaffold(  
-      appBar: buildMainAppBar("Music League: $leagueName"), 
-      body: Center(  
-        child: SingleChildScrollView(  
-          child: Column( 
-            children: [
-              Text(
-                "Statistics for $playerName", 
-                style: TextStyle( 
-                  fontSize: 24, 
-                  fontWeight: FontWeight.bold, 
-                )
-              ),
+      appBar: buildMainAppBar("Music League: ${widget.leagueName}"), 
+      body: FutureBuilder<Map<String, dynamic>> ( 
+        future: _data, 
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) { 
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) { 
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          final stats = snapshot.data!; 
+          if (!stats.containsKey("people")) {
+            return const Center(child: Text("Invalid JSON format: missing 'people'!")); 
+          }
+          final people = stats["people"] as List<dynamic>; 
 
-              SizedBox(height: 50), 
+          return ListView(  
+            padding: const EdgeInsets.all(16), 
+            children: people.map<Widget>( (person) {
+              final personMap = person as Map<String, dynamic>; 
+              final scores = personMap["scores"] as Map<String, dynamic>; 
 
-              Center( 
-                child: Container( 
-                  padding: const EdgeInsets.all(16), 
-                  color: Colors.blue[50], 
-                  child: SizedBox( 
-                    height: 300, 
-                    child: LineChart( 
-                      LineChartData( 
-                        borderData: FlBorderData(show: true), 
-                        titlesData: FlTitlesData( 
-                          bottomTitles: AxisTitles( 
-                            sideTitles: SideTitles( 
-                              showTitles: true, 
-                              getTitlesWidget: (value, meta) { 
-                                return Text("R${value.toInt() + 1}");
-                              },
-                            ),
-                          ),
-                          leftTitles: AxisTitles( 
-                            sideTitles: SideTitles( showTitles: true), 
-                          ),
-                        ) ,
-                        lineBarsData: [
-                          LineChartBarData( 
-                            spots: List.generate( 
-                              points.length, 
-                              (index) => 
-                                  FlSpot(index.toDouble(), points[index].toDouble())), 
-                            isCurved: true, 
-                            color: Colors.deepPurple, 
-                            barWidth: 3, 
-                            dotData: FlDotData(show: true), 
-                          ),
-                        ],
-                      ),
-                    ),
+              // final name = entry.key; 
+              // final personStats = entry.value as Map<String, dynamic>; 
+              return Card(  
+                margin: const EdgeInsets.symmetric(vertical: 8), 
+                child: ListTile(   
+                  title: Text(personMap["name"], style: const TextStyle(fontWeight: FontWeight.bold)), 
+                  subtitle: Text(  
+                    "\t\tBiggest Fan: ${scores['biggestFanName']} (${scores['biggestFanScore']} votes)\n" 
+                    "\t\tBiggest Hater: ${scores['biggestHaterName']} (${scores['biggestHaterScore']} votes)\n" 
+                    "\t\tMost Liked: ${scores['mostLikedName']} (${scores['mostLikedScore']} votes)\n" 
+                    "\t\tLeast Liked: ${scores['leastLikedName']} (${scores['leastLikedScore']} votes)" 
                   ),
                 ),
-              ),
-
-              Text("Points Received: "), 
-              Text("[Plot of points per round?]"), 
-              buildStatistic("Most Liked Player: "), 
-              buildStatistic("Least Liked Player: "), 
-              buildStatistic("Biggest Fan: "), 
-              buildStatistic("Biggest Hater: "), 
-              buildStatistic("Most Similar Taste: "),
-            ]
-          ),
-        ),
-      ),
-    );
+              );
+            }).toList(),
+          );
+        }, 
+      ), 
+    ); 
   }
 }
+          
+      //     Center(  
+      //   child: SingleChildScrollView(  
+      //     child: Column( 
+      //       children: [
+      //         Text(
+      //           "Statistics for $playerName", 
+      //           style: TextStyle( 
+      //             fontSize: 24, 
+      //             fontWeight: FontWeight.bold, 
+      //           )
+      //         ),
+
+      //         SizedBox(height: 50), 
+
+      //         Center( 
+      //           child: Container( 
+      //             padding: const EdgeInsets.all(16), 
+      //             color: Colors.blue[50], 
+      //             child: SizedBox( 
+      //               height: 300, 
+      //               child: LineChart( 
+      //                 LineChartData( 
+      //                   borderData: FlBorderData(show: true), 
+      //                   titlesData: FlTitlesData( 
+      //                     bottomTitles: AxisTitles( 
+      //                       sideTitles: SideTitles( 
+      //                         showTitles: true, 
+      //                         getTitlesWidget: (value, meta) { 
+      //                           return Text("R${value.toInt() + 1}");
+      //                         },
+      //                       ),
+      //                     ),
+      //                     leftTitles: AxisTitles( 
+      //                       sideTitles: SideTitles( showTitles: true), 
+      //                     ),
+      //                   ) ,
+      //                   lineBarsData: [
+      //                     LineChartBarData( 
+      //                       spots: List.generate( 
+      //                         points.length, 
+      //                         (index) => 
+      //                             FlSpot(index.toDouble(), points[index].toDouble())), 
+      //                       isCurved: true, 
+      //                       color: Colors.deepPurple, 
+      //                       barWidth: 3, 
+      //                       dotData: FlDotData(show: true), 
+      //                     ),
+      //                   ],
+      //                 ),
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+
+      //         Text("Points Received: "), 
+      //         Text("[Plot of points per round?]"), 
+      //         buildStatistic("Most Liked Player (include 2nd and 3rd / all in drop down): "), 
+      //         buildStatistic("Least Liked Player: "), 
+      //         buildStatistic("Biggest Fan: "), 
+      //         buildStatistic("Biggest Hater: "), 
+      //         buildStatistic("Most Similar Taste: "),
+      //       ]
+      //     ),
+      //   ),
+      // ),
+//     );
+//   }
+// }
 
 Text buildStatistic(String title) {
   return Text(  
@@ -283,31 +340,31 @@ Text buildStatistic(String title) {
   );
 }
 
-Card buildInkwellCard(String leagueName, BuildContext context) {
-    return Card(  
-      child: InkWell( 
-        onTap: () {
-          Navigator.push( 
-            context, 
-            MaterialPageRoute( 
-              builder: (context) => LeaguePage(leagueName: leagueName), 
-            ),
-          );
-        },
-        hoverColor: const Color.fromARGB(255, 197, 230, 199),
-        child: Center(  
-          child: Text(  
-            leagueName, 
-            style: TextStyle(  
-              fontSize: 18, 
-              fontWeight: FontWeight.bold
-            ), 
-            textAlign: TextAlign.center, 
-          ),
-        ),
-      ),
-    );
-}
+// Card buildInkwellCard(String leagueName, BuildContext context) {
+//     return Card(  
+//       child: InkWell( 
+//         onTap: () {
+//           Navigator.push( 
+//             context, 
+//             MaterialPageRoute( 
+//               builder: (context) => LeaguePage(leagueName: leagueName), 
+//             ),
+//           );
+//         },
+//         hoverColor: const Color.fromARGB(255, 197, 230, 199),
+//         child: Center(  
+//           child: Text(  
+//             leagueName, 
+//             style: TextStyle(  
+//               fontSize: 18, 
+//               fontWeight: FontWeight.bold
+//             ), 
+//             textAlign: TextAlign.center, 
+//           ),
+//         ),
+//       ),
+//     );
+// }
 
 AppBar buildMainAppBar(String title) {
   return AppBar( 
@@ -324,17 +381,22 @@ AppBar buildMainAppBar(String title) {
   );
 }
 
-Future<List<List<dynamic>>> loadLeagueRound(String league, String roundFile) async {
-  final path = 'assets/data/$league/$roundFile';
-  final rawData = await rootBundle.loadString(path); 
-  return const CsvToListConverter().convert(rawData);
+// Future<List<List<dynamic>>> loadLeagueRound(String league, String roundFile) async {
+//   final path = 'assets/data/$league/$roundFile';
+//   final rawData = await rootBundle.loadString(path); 
+//   return const CsvToListConverter().convert(rawData);
+// }
+
+// final leagueRounds = {
+//   "virginia_is_for_music_lovers" : ["round1.csv", "round2.csv"], 
+//   "live_the_riv" : ["round1.csv", "round2.csv"], 
+//   "granny_smith" : ["round1.csv", "round2.csv"],
+// };
+
+Future<Map<String, dynamic>> loadStatsJson(String league) async { 
+  final path = 'assets/data/$league.json';
+  final rawData = await rootBundle.loadString(path);
+  return jsonDecode(rawData) as Map<String, dynamic>;
 }
-
-final leagueRounds = {
-  "virginia_is_for_music_lovers" : ["round1.csv", "round2.csv"], 
-  "live_the_riv" : ["round1.csv", "round2.csv"], 
-  "granny_smith" : ["round1.csv", "round2.csv"],
-};
-
 
 
